33612964ac95ea7542f930e73295fd9a87a6ac3b
Zhivaev Artem <zhivaev993@gmail.com>
2019-03-13 14:47:53 +0300

    main

--- samples/main.cpp
+++ samples/main.cpp
@@ -5,19 +5,134 @@
 //
 
 #include <iostream>
+#include <cstdio>
+#include <limits>
 #include "include/ttext.h"
 
 using namespace std;
 
+typedef void __proc();
+typedef __proc *procedure;
+
+procedure CLEAR;
+void PAUSE();
+void __uniCLEAR();
+void __winCLEAR();
+
+void insert();
+void remove();
+void setl();
+
 int main()
 {
-  TText t;
-  t.Read("../test/text2.txt");
-  t.Print();
+  CLEAR = __uniCLEAR;
+  PTText pt = NULL;
+  char choice;
+  bool quit = false;
 
-  t.Reset();
-  t.DelNextSection();
+  while(true)
+  {
+    CLEAR();
+    cout
+        << "1. Enter path\n"
+        << "2. Show text\n"
+        << "3. Insert line\n"
+        << "4. Set line\n"
+        << "5. Delete line\n"
+        << "0. Exit\n"
+        << flush;
+
+    cin >> choice;
+    cin.ignore(numeric_limits<std::streamsize>::max(), '\n');
+    cin.clear();
+
+    switch(choice)
+    {
+    case '1':
+      char pth[256];
+      CLEAR();
+      cout << "Enter path" << endl;
+      cin.getline(pth, 256);
+      try
+      {
+        if(pt == NULL)
+          pt = new TText;
+        pt->Read(pth);
+      }
+      catch(...)
+      {
+        delete pt;
+        pt = NULL;
+        cout
+            << "Invalid path\n"
+            << "Press enter...\n"
+            << flush;
+        PAUSE();
+      }
+      break;
+
+    case '2':
+      CLEAR();
+      if(pt == NULL)
+        cout << "Select file\n";
+      else
+        pt->Print();
+      cout << "\nPress enter...\n";
+      PAUSE();
+      break;
+
+    case '3':
+      insert();
+      break;
+
+    case '4':
+      break;
+
+    case '5':
+      break;
+
+    case '0':
+      quit = true;
+      break;
+
+    default:
+      break;
+    };
+
+    if(quit)
+      break;
+  }
 
   return 0;
+}
+
+
+
+void __winCLEAR()
+{
+  system("cls");
+}
+
+void __uniCLEAR()
+{
+  system("clear");
+}
+
+void PAUSE()
+{
+  char ch;
+  while((ch = getchar()) != '\n');
+}
+
+void insert()
+{
+}
+
+void remove()
+{
+}
+
+void setl()
+{
 }
 
