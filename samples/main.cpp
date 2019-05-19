28bb8171a4bb49bf8ab37bcd3ec4aa365cf6131c
PinDanil <drobrorobro@gmail.com>
2019-05-19 15:01:18 +0300

    Add main()

--- samples/main.cpp
+++ samples/main.cpp
@@ -5,10 +5,31 @@
 //
 
 #include <iostream>
-
+#include "text.h"
+#include "textlink.h"
 using namespace std;
 
 int main()
 {
-  return 0;
+	TTextLink::InitMemSystem();
+	TText txt;
+
+	txt.SetLine("Lable 1");
+	txt.InsDownLine("txt 1");
+	txt.GoDownLink();
+	txt.InsNextLine("txt 2");
+
+	txt.GoFirstLink();
+
+	txt.InsNextLine("Lable 2");
+	txt.GoNextLink();
+	txt.InsDownLine("txt 1");
+	txt.GoDownLink();
+	txt.InsNextLine("txt 2");
+
+	txt.Print();
+
+	PTText txt2 = txt.GetCopy();
+	txt2->Print();
+	return 0;
 }
