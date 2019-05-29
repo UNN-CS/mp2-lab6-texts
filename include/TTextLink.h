64f0d80bc4eeda9bbe8fb9b9420672e751389697
Михаил Грибанов <muchroom4427@yandex.ru>
2019-05-30 00:40:11 +0300

    Added TTextLink

--- include/TTextLink.h
+++ include/TTextLink.h
@@ -1,54 +1,56 @@
-﻿#ifndef __T_TEXT_LINK_H__
-#define __T_TEXT_LINK_H__
+﻿#ifndef __TEXTLINK_H
+#define __TEXTLINK_H
 
-#include <string.h>
+#include <string>
 #include "tdatvalue.h"
-
-#define TextLineLength 20
-#define MemSize        20
-
-#define TextError 101
-#define TextNoPrev 102
+#include <iostream>
+#include <cstring>
+#include "ttext.h"
 
 class TText;
 class TTextLink;
-typedef TTextLink* PTTextLink;
+typedef TTextLink * PTTextLink;
 typedef char TStr[TextLineLength];
 
-class TTextMem {
+class TTextMem
+{
 	PTTextLink pFirst;
 	PTTextLink pLast;
 	PTTextLink pFree;
 	friend class TTextLink;
 };
-typedef TTextMem *PTTextMem;
+typedef TTextMem * PTTextMem;
 
-class TTextLink : public TDatValue {
+class TTextLink : public TDatValue
+{
 protected:
 	TStr Str;
-	PTTextLink pNext, pDown; 
-	static TTextMem MemHeader;
+	PTTextLink pNext, pDown;
+	static  PTTextMem MemHeader;
+
 public:
 	static void InitMemSystem(int size = MemSize);
 	static void PrintFreeLink();
 	void * operator new (size_t size);
-	void operator delete (void *pM);
+	void operator delete(void *pM);
 	static void MemCleaner(TText &txt);
 	TTextLink(TStr s = nullptr, PTTextLink pn = nullptr, PTTextLink pd = nullptr)
 	{
-		pNext = pn; pDown = pd;
+		pNext = pn;
+		pDown = pd;
 		if (s != nullptr)
 			strcpy(Str, s);
-		else Str[0] = '\0';
+		else
+			Str[0] = '\0';
 	}
 	~TTextLink() {}
-	int IsAtom() { return pDown == nullptr; }
+	bool IsAtom() { return pDown == nullptr; }
 	PTTextLink GetNext() { return pNext; }
 	PTTextLink GetDown() { return pDown; }
-	PTDatValue GetCopy() { return new TTextLink(Str, pNext, pDown); }
+	TDatValue * GetCopy() { return new TTextLink(Str, pNext, pDown); }
+
 protected:
 	virtual void Print(std::ostream &os) { os << Str; }
 	friend class TText;
 };
-
-#endif // __T_TEXT_LINK_H__
+#endif
