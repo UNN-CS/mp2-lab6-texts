a1c5ce9ecdb2ac0cfdb9b0d11ed3e99896b53fc3
Михаил Грибанов <muchroom4427@yandex.ru>
2019-05-30 00:00:53 +0300

    Added headers

---
+++ include/TText.h
@@ -0,0 +1,53 @@
+#ifndef __T_TEXT_H__
+#define __T_TEXT_H__
+
+#include "ttextlink.h"
+#include <stack>
+#include <iostream>
+#include <cstring>
+
+class TText;
+typedef TText * PTText;
+
+class TText {
+protected:
+	PTTextLink pFirst;      // указатель корня дерева
+	PTTextLink pCurrent;      // указатель текущей строки
+	std::stack< PTTextLink > Path; // стек траектории движения по тексту
+	std::stack< PTTextLink > St;   // стек для итератора
+	PTTextLink GetFirstAtom(PTTextLink pl); // поиск первого атома
+	void PrintText(PTTextLink ptl);         // печать текста со звена ptl
+	PTTextLink ReadText(std::ifstream &TxtFile); //чтение текста из файла
+public:
+	TText(PTTextLink pl = nullptr);
+	~TText() { pFirst = nullptr; }
+	PTText GetCopy();
+	// навигация
+	void GoFirstLink(); // переход к первой строке
+	void GoDownLink();  // переход к следующей строке по Down
+	void GoNextLink();  // переход к следующей строке по Next
+	void GoPrevLink();  // переход к предыдущей позиции в тексте
+						// доступ
+	std::string GetLine();   // чтение текущей строки
+	void SetLine(std::string s); // замена текущей строки
+								 // модификация
+	void InsDownLine(std::string s);    // вставка строки в подуровень
+	void InsDownSection(std::string s); // вставка раздела в подуровень
+	void InsNextLine(std::string s);    // вставка строки в том же уровне
+	void InsNextSection(std::string s); // вставка раздела в том же уровне
+	void DelDownLine();        // удаление строки в подуровне
+	void DelDownSection();     // удаление раздела в подуровне
+	void DelNextLine();        // удаление строки в том же уровне
+	void DelNextSection();     // удаление раздела в том же уровне
+							   // итератор
+	int Reset();              // установить на первую звапись
+	int IsTextEnded() const;  // текст завершен?
+	int GoNext();             // переход к следующей записи
+							  //работа с файлами
+	void Read(const char * pFileName);  // ввод текста из файла
+	void Write(char * pFileName); // вывод текста в файл
+								  //печать
+	void Print();             // печать текста
+};
+
+#endif // __T_TEXT_H__
---
+++ include/TTextLink.h
@@ -0,0 +1,54 @@
+﻿#ifndef __T_TEXT_LINK_H__
+#define __T_TEXT_LINK_H__
+
+#include <string.h>
+#include "tdatvalue.h"
+
+#define TextLineLength 20
+#define MemSize        20
+
+#define TextError 101
+#define TextNoPrev 102
+
+class TText;
+class TTextLink;
+typedef TTextLink* PTTextLink;
+typedef char TStr[TextLineLength];
+
+class TTextMem {
+	PTTextLink pFirst;
+	PTTextLink pLast;
+	PTTextLink pFree;
+	friend class TTextLink;
+};
+typedef TTextMem *PTTextMem;
+
+class TTextLink : public TDatValue {
+protected:
+	TStr Str;
+	PTTextLink pNext, pDown; 
+	static TTextMem MemHeader;
+public:
+	static void InitMemSystem(int size = MemSize);
+	static void PrintFreeLink();
+	void * operator new (size_t size);
+	void operator delete (void *pM);
+	static void MemCleaner(TText &txt);
+	TTextLink(TStr s = nullptr, PTTextLink pn = nullptr, PTTextLink pd = nullptr)
+	{
+		pNext = pn; pDown = pd;
+		if (s != nullptr)
+			strcpy(Str, s);
+		else Str[0] = '\0';
+	}
+	~TTextLink() {}
+	int IsAtom() { return pDown == nullptr; }
+	PTTextLink GetNext() { return pNext; }
+	PTTextLink GetDown() { return pDown; }
+	PTDatValue GetCopy() { return new TTextLink(Str, pNext, pDown); }
+protected:
+	virtual void Print(std::ostream &os) { os << Str; }
+	friend class TText;
+};
+
+#endif // __T_TEXT_LINK_H__
