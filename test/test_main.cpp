73b01c4637a6ab0a7c21d49854065dc91e08ff6a
utshomomen <32203690+utshomomen@users.noreply.github.com>
2019-06-02 18:55:41 +0300

    v.2

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,7 +1,5 @@
-#include <gtest.h>
-int main(int argc, char **argv)
-{
+#include <gtest/gtest.h>
+int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
-
 }
